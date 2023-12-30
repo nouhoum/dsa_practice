595e8952686bf4e3c32d3aeaca154dc661921ab1
Nouhoum TRAORE <nouhoum.traore@gmail.com>
2023-12-30 18:32:46 +0100

    feat: singly linked list insert and to string

---
+++ pkg/ds/singly_linked_list.go
@@ -0,0 +1,80 @@
+package ds
+
+import (
+	"fmt"
+	"strings"
+)
+
+type node struct {
+	data int64
+	next *node
+}
+
+type singlyLinkedList struct {
+	head *node
+}
+
+// Get implements List.
+func (*singlyLinkedList) Get(i int64) (int64, error) {
+	panic("unimplemented")
+}
+
+// Insert implements List.
+func (ll *singlyLinkedList) Insert(e int64) {
+	newNode := &node{data: e}
+	if ll.head == nil {
+		ll.head = newNode
+		return
+	}
+	newNode.next = ll.head
+	ll.head = newNode
+}
+
+// InsertAt implements List.
+func (*singlyLinkedList) InsertAt(i int64, e int64) {
+	panic("unimplemented")
+}
+
+// IsEmpty implements List.
+func (*singlyLinkedList) IsEmpty() bool {
+	panic("unimplemented")
+}
+
+// Len implements List.
+func (*singlyLinkedList) Len() int64 {
+	panic("unimplemented")
+}
+
+// RemoveAt implements List.
+func (*singlyLinkedList) RemoveAt(i int64) bool {
+	panic("unimplemented")
+}
+
+// String implements List.
+func (ll *singlyLinkedList) String() string {
+	if ll.head == nil {
+		return "[]"
+	}
+	sb := strings.Builder{}
+	sb.WriteString("[")
+	curr := ll.head
+	for curr != nil {
+		sb.WriteString(fmt.Sprint(curr.data))
+		curr = curr.next
+		if curr != nil {
+			sb.WriteString(" ")
+		}
+	}
+
+	sb.WriteString("]")
+	return sb.String()
+}
+
+// UpdateAt implements List.
+func (*singlyLinkedList) UpdateAt(i int64, e int64) error {
+	panic("unimplemented")
+}
+
+func NewSinglyLinkedList() List {
+	return &singlyLinkedList{}
+}
---
+++ pkg/ds/singly_linked_list_test.go
@@ -0,0 +1,89 @@
+package ds_test
+
+import (
+	"testing"
+
+	"github.com/nouhoum/dsa_practice/pkg/ds"
+)
+
+func TestSinglyLinkedList_String(t *testing.T) {
+	// Test case 1: Empty list
+	ll := ds.NewSinglyLinkedList()
+	expected := "[]"
+	if result := ll.String(); result != expected {
+		t.Errorf("Expected %s, but got %s", expected, result)
+	}
+
+	// Test case 2: Non-empty list
+	ll = ds.NewSinglyLinkedList()
+	ll.Insert(1)
+
+	expected = "[1]"
+	if result := ll.String(); result != expected {
+		t.Errorf("Expected %s, but got %s", expected, result)
+	}
+
+	ll.Insert(2)
+	expected = "[2 1]"
+	if result := ll.String(); result != expected {
+		t.Errorf("Expected %s, but got %s", expected, result)
+	}
+}
+
+// func TestSinglyLinkedList_Insert(t *testing.T) {
+// 	tests := []struct {
+// 		name string
+// 		list ds.List
+// 		e    int64
+// 	}{
+// 		{
+// 			name: "Inserting into an empty list",
+// 			list: ds.NewSinglyLinkedList(),
+// 			e:    1,
+// 		},
+// 		{
+// 			name: "Inserting into a non-empty list",
+// 			list: singlyLinkedList{
+// 				head: &node{value: 1},
+// 			},
+// 			e: 2,
+// 		},
+// 	}
+
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			tt.list.Insert(tt.e)
+// 			// Add assertions here to check if the element was inserted correctly.
+// 		})
+// 	}
+// }
+
+// func TestSinglyLinkedList_Get(t *testing.T) {
+// 	// Test case 1: Getting an element at a valid index
+// 	// list := &singlyLinkedList{1, 2, 3, 4, 5}
+// 	list := ds.NewSinglyLinkedList()
+// 	expected := int64(3)
+// 	result, err := list.Get(2)
+// 	if err != nil {
+// 		t.Errorf("Expected no error, got %v", err)
+// 	}
+// 	if result != expected {
+// 		t.Errorf("Expected %v, got %v", expected, result)
+// 	}
+
+// 	// Test case 2: Getting an element at a negative index
+// 	// list = &singlyLinkedList{1, 2, 3, 4, 5}
+// 	list = ds.NewSinglyLinkedList()
+// 	_, err = list.Get(-1)
+// 	if err == nil {
+// 		t.Errorf("Expected an error, got nil")
+// 	}
+
+// 	// Test case 3: Getting an element at an index greater than the length of the list
+// 	// list = &singlyLinkedList{1, 2, 3, 4, 5}
+// 	list = ds.NewSinglyLinkedList()
+// 	_, err = list.Get(10)
+// 	if err == nil {
+// 		t.Errorf("Expected an error, got nil")
+// 	}
+// }
